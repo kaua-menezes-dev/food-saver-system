96461798663b3c70c88cece01b89a4550637b507
Kauã Menezes <kauavictor.f.menezes@hotmail.com>
2026-03-15 15:31:13 -0300

    feat: registrar alimento na classe FoodService

--- src/com/projeto/foodSaver/service/FoodService.java
+++ src/com/projeto/foodSaver/service/FoodService.java
@@ -1,5 +1,6 @@
 package com.projeto.foodSaver.service;
 
+import com.projeto.foodSaver.model.Food;
 import com.projeto.foodSaver.repository.FoodRepository;
 
 public class FoodService {
@@ -7,5 +8,14 @@ public class FoodService {
 
     public FoodService(FoodRepository repository) {
         this.repository = repository;
+    }
+
+    public void registerFood(String name, String category, int quantity) {
+        try {
+            var newFood = new Food(name, category, quantity);
+            repository.saveFood(newFood);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
