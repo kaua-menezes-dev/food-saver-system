702b29df635ddde4ff4056c557966192dd6ba72e
Kauã Menezes <kauavictor.f.menezes@hotmail.com>
2026-03-15 15:33:37 -0300

    feat: exibir todos os alimentos da lista na classe FoodService

--- src/com/projeto/foodSaver/service/FoodService.java
+++ src/com/projeto/foodSaver/service/FoodService.java
@@ -3,6 +3,8 @@ package com.projeto.foodSaver.service;
 import com.projeto.foodSaver.model.Food;
 import com.projeto.foodSaver.repository.FoodRepository;
 
+import java.util.List;
+
 public class FoodService {
     FoodRepository repository;
 
@@ -16,6 +18,19 @@ public class FoodService {
             repository.saveFood(newFood);
         } catch (Exception e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    public void showAllFoods() {
+        try {
+            List<Food> foods = repository.listAllFood();
+
+            for (Food food : foods) {
+                System.out.println(food);
+            }
+
+        } catch (Exception e) {
+            System.out.println("It's impossible to show an empty list");
         }
     }
 }
