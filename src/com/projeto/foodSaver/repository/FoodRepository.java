218271cccfd55247161e9687e56a9a1569cb96ab
Kauã Menezes <kauavictor.f.menezes@hotmail.com>
2026-03-15 15:21:20 -0300

    feat: criar classe FoodRepository

---
+++ src/com/projeto/foodSaver/repository/FoodRepository.java
@@ -0,0 +1,10 @@
+package com.projeto.foodSaver.repository;
+
+import com.projeto.foodSaver.model.Food;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FoodRepository {
+    List<Food> foods = new ArrayList<>();
+}
